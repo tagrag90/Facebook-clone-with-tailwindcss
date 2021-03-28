6a9e5963ce7800ee34f4fef1d6bf822ad5e187e4
tagrag90 <gunjeepark21@gmail.com>
2021-03-28 23:07:01 +0900

    set up navbar

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    ".app/helpers/**/*.rb",
+    ".app/javascript/**/*.js",
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "dark-main": "#18191A",
+        "dark-second": "#242526",
+        "dark-third": "#3A3B3C",
+        "dark-txt": "#B8BBBF",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      transform: ["group-hover"],
+      scale: ["group-hover"],
+    },
+  },
+  plugins: [],
+};
